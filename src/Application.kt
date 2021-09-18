57ab4637dbe024b1fcec67450241f43634493a8f
MilicTG <milic.tg@gmail.com>
2021-09-18 22:26:26 +0200

    Install some features

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,28 @@
 package dev.milic
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    //Some info about server
+    install(DefaultHeaders)
+    //Log all HTTP requests and responses
+    install(CallLogging)
+    //Define URL endpoints
+    install(Routing)
+    //Define a response type JSON
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
 }
 
